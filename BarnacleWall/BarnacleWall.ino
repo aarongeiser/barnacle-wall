65cd33394780d299d2e8695e95989814ffb2e7f3
Aaron Geiser <aarongeiser@gmail.com>
2017-11-16 17:05:38 -0600

    Add touch sensors

--- BarnacleWall/BarnacleWall.ino
+++ BarnacleWall/BarnacleWall.ino
@@ -1,101 +1,158 @@
 #include <FastLED.h>
 #define NUM_LEDS 398
-#define NUM_CLUSTERS 43
+#define NUM_BARNACLES 43
+#define NUM_SENSORS 7
 #define DATA_PIN 0
-#define BRIGHTNESS  240
-#define FRAMES_PER_SECOND 10
+#define BRIGHTNESS  255
+#define FRAMES_PER_SECOND 60
 
 CRGB leds[NUM_LEDS];
 
-// Barnacle array
-// 1 == small/large | 2 == medium
-int barnacles[44][2] = {
-  {1, 0},   // 1
-  {1, 10},  // 2
-  {1, 20},  // 3
-  {1, 27},  // 4
-  {1, 34},  // 5
-  {1, 41},  // 6
-  {1, 50},  // 7
-  {1, 57},   // 8
-  {0, 65},   // 9
-  {1, 81},   // 10
-  {0, 92},   // 11
-  {1, 105},   // 12
-  {0, 112},   // 13
-  {0, 125},   // 14
-  {1, 138},   // 15
-  {1, 145},   // 16
-  {1, 152},   // 17
-  {1, 159},   // 18
-  {0, 166},   // 19
-  {1, 180},   // 20
-  {1, 190},   // 21
-  {0, 197},   // 22
-  {0, 210},   // 23
-  {1, 223},   // 24
-  {1, 232},   // 25
-  {1, 239},   // 26
-  {1, 246},   // 27
-  {1, 253},   // 28
-  {1, 260},   // 29
-  {1, 267},   // 30
-  {0, 274},   // 31
-  {1, 287},   // 32
-  {1, 295},   // 33
-  {1, 305},   // 34
-  {0, 312},   // 35
-  {1, 325},   // 36
-  {1, 332},   // 37
-  {0, 339},   // 38
-  {0, 352},   // 39
-  {1, 365},   // 40
-  {1, 372},   // 41
-  {1, 379},   // 42
-  {1, 386},   // 43
-  {1, 392},   // 44
+class Barnacle {
+  public:
+    int type;             // sm = 1, md = 2, lg = 3
+    int center;           // center LED
+    CHSV primaryColor;    // fiber optic color
+    CHSV secondaryColor;  // body color
+
+    // Constructor
+    Barnacle(int _type, int _center, CHSV _primaryColor, CHSV _secondaryColor)
+    {
+      type = _type;
+      center = _center;
+      primaryColor = _primaryColor;
+      secondaryColor = _secondaryColor;
+    }
+
+    /**
+     * Update Barnacle
+     */
+    void setCluster(CHSV primary, CHSV secondary)
+    {
+      int center = this->center;
+      this->primaryColor = primary;
+      this->secondaryColor = secondary;
+      
+      if (this->type == 2) {
+        leds[center] = primary;
+        leds[(center + 2)] = secondary;
+        leds[(center + 4)] = secondary;
+        leds[(center + 6)] = secondary;
+        leds[(center + 8)] = secondary;
+        leds[(center + 10)] = secondary;
+        leds[(center + 12)] = secondary;
+      } else {
+        leds[center] = primary;
+        leds[(center + 1)] = secondary;
+        leds[(center + 2)] = secondary;
+        leds[(center + 3)] = secondary;
+        leds[(center + 4)] = secondary;
+        leds[(center + 5)] = secondary;
+        leds[(center + 6)] = secondary;
+      }
+    }
 };
+
+class Sensor {
+  public:
+    int pin;      // Pin number
+    int state;    // 0 = off, 1 = on
+    int pressed;  // Is pressed?
+
+    // Constructor
+    Sensor(int _pin, int _state, _pressed)
+    {
+      pin = _pin;
+      state = _state;
+      pressed = _pressed;
+    }
+};
+
+// Barnacle array
+Barnacle *barnacles[44];
+
+// Sensor array
+Sensor *sensors[7];
 
 // Section array
 int sections[7][2] = {
-  {0, 6},   // 1
-  {6, 11},  // 2
-  {10, 16}, // 3
-  {16, 23}, // 4
-  {21, 28}, // 5
-  {27, 34}, // 6
-  {35, 43}  // 7
+  {0, 6},   // 0
+  {6, 11},  // 1
+  {10, 16}, // 2
+  {16, 23}, // 3
+  {21, 28}, // 4
+  {27, 34}, // 5
+  {35, 43}  // 6
 };
 
-// Buttons
-const int buttonPin01 = 4;
-const int buttonPin02 = 5;
-const int buttonPin03 = 6;
-const int sensorPin01 = 7;
-
-// Sensors
-const int sensorPin02 = 8;
-const int sensorPin03 = 9;
-const int sensorPin04 = 10;
-const int sensorPin05 = 11;
-const int sensorPin06 = 12;
-const int sensorPin07 = 13;
-
-// variables will change:
-bool gReverseDirection = false;
-int buttonPressed = 0;
-int previousButton = 0;
-int ledMode = 0;
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+// rotating "base color" used by many of the patterns
+uint8_t gHue = 0; 
 
 void setup() {
   Serial.begin(9600);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin01, INPUT_PULLUP);
-  pinMode(buttonPin02, INPUT_PULLUP);
-  pinMode(buttonPin03, INPUT_PULLUP);
+
+  // Sensor mapping
+  sensors[0] = new Sensor(4, 0);
+  sensors[1] = new Sensor(5, 0);
+  sensors[2] = new Sensor(6, 0);
+  sensors[3] = new Sensor(7, 0);
+  sensors[4] = new Sensor(8, 0);
+  sensors[5] = new Sensor(9, 0);
+  sensors[6] = new Sensor(10, 0);
+
+  // Enable pins
+  for(int i = 0; i < NUM_SENSORS; ++i) {
+    pinMode(sensors[i]->pin, INPUT_PULLUP);
+  } 
+
+  // Barnacle mapping
+  barnacles[0] =  new Barnacle(1, 0, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[1] =  new Barnacle(1, 10, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[2] =  new Barnacle(1, 20, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[3] =  new Barnacle(1, 27, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[4] =  new Barnacle(3, 34, CHSV(64, 255, 255), CHSV(64, 255, 255));  // Large
+  barnacles[5] =  new Barnacle(1, 41, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[6] =  new Barnacle(3, 50, CHSV(64, 255, 255), CHSV(64, 255, 255));  // Large
+  barnacles[7] =  new Barnacle(1, 57, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[8] =  new Barnacle(2, 65, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[9] =  new Barnacle(1, 81, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[10] = new Barnacle(2, 92, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[11] = new Barnacle(3, 105, CHSV(64, 255, 255), CHSV(64, 255, 255)); // Large
+  barnacles[12] = new Barnacle(2, 112, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[13] = new Barnacle(2, 125, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[14] = new Barnacle(1, 138, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[15] = new Barnacle(1, 145, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[16] = new Barnacle(1, 152, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[17] = new Barnacle(3, 159, CHSV(64, 255, 255), CHSV(64, 255, 255)); // Large
+  barnacles[18] = new Barnacle(2, 166, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[19] = new Barnacle(1, 180, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[20] = new Barnacle(1, 190, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[21] = new Barnacle(2, 197, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[22] = new Barnacle(2, 210, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[23] = new Barnacle(1, 223, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[24] = new Barnacle(1, 232, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[25] = new Barnacle(3, 239, CHSV(64, 255, 255), CHSV(64, 255, 255)); // Large
+  barnacles[26] = new Barnacle(2, 246, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[27] = new Barnacle(1, 253, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[28] = new Barnacle(3, 260, CHSV(64, 255, 255), CHSV(64, 255, 255)); // Large
+  barnacles[29] = new Barnacle(1, 267, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[30] = new Barnacle(2, 274, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[31] = new Barnacle(1, 287, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[32] = new Barnacle(1, 295, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[33] = new Barnacle(1, 305, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[34] = new Barnacle(2, 312, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[35] = new Barnacle(3, 325, CHSV(64, 255, 255), CHSV(64, 255, 255)); // Large
+  barnacles[36] = new Barnacle(1, 332, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[37] = new Barnacle(2, 339, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[38] = new Barnacle(2, 352, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[39] = new Barnacle(1, 365, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[40] = new Barnacle(1, 372, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[41] = new Barnacle(1, 379, CHSV(64, 255, 255), CHSV(64, 255, 255));
+  barnacles[42] = new Barnacle(1, 386, CHSV(64, 255, 255), CHSV(64, 255, 255));
+
+  // Initialize LEDs
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness( BRIGHTNESS );
+  FastLED.setBrightness(BRIGHTNESS);
   FastLED.clear();
 }
 
@@ -104,6 +161,7 @@ void setup() {
  */
 void loop() {
   checkInputs();
+  ambient();
   renderEffects();
   FastLED.show();
   FastLED.delay(1000/FRAMES_PER_SECOND); 
@@ -112,172 +170,54 @@ void loop() {
 /**
  * Check our inputs and set the button state
  */
-void checkInputs() {
+void checkInputs() 
+{
   // Read pins
-  if (digitalRead(buttonPin01) == LOW) {
-    buttonPressed = buttonPin01;
-    return buttonPressed;
+  for(int i = 0; i < NUM_SENSORS; ++i) {
+    if (digitalRead(sensors[i]->pin) == HIGH) {
+      sensors[i]->state = 1;
+    } else {
+      sensors[i]->state = 0;
+    } 
   } 
-  
-  if (digitalRead(buttonPin02) == LOW) {
-    buttonPressed = buttonPin02;
-    return buttonPressed;
-  } 
-  
-  if (digitalRead(buttonPin03) == LOW) {
-    buttonPressed = buttonPin03;
-    return buttonPressed;
-  } 
-
-  buttonPressed = 0;
 }
 
 /**
- * Handle the button state to render effects
+ * Handle the button states to render effects
  */
 void renderEffects() {
-    switch (buttonPressed) {
-    case buttonPin01:
-      Serial.println("Button 1 pressed.");  
-      // rainbowWithGlitter();
-      test();
-      break;
-    case buttonPin02:
-      Serial.println("Button 2 pressed.");  
-      bpm();
-      break;
-    case buttonPin03:
-      Serial.println("Button 3 pressed."); 
-      // juggle();
-      background();
-      break;
-    case 0:
-      Serial.println("No button pressed.");  
-      int sec = random8(0, 7);
-      illuminate(sec);
-      break;
-  }
-  background();
+  // Render effects based on sensors touched
+  for(int i = 0; i < NUM_SENSORS; ++i) {
+    if (digitalRead(sensors[i]->state) == 1) {
+      illuminate(sections[i]);
+    }
+  } 
 }
 
-void setCluster(int cluster[], CHSV primary, CHSV secondary)
-{
-  if (cluster[0] == 1) {
-    leds[cluster[1]] = primary;
-    leds[(cluster[1] + 1)] = secondary;
-    leds[(cluster[1] + 2)] = secondary;
-    leds[(cluster[1] + 3)] = secondary;
-    leds[(cluster[1] + 4)] = secondary;
-    leds[(cluster[1] + 5)] = secondary;
-    leds[(cluster[1] + 6)] = secondary;
-  } else {
-    leds[cluster[1]] = primary;
-    leds[(cluster[1] + 2)] = secondary;
-    leds[(cluster[1] + 4)] = secondary;
-    leds[(cluster[1] + 6)] = secondary;
-    leds[(cluster[1] + 8)] = secondary;
-    leds[(cluster[1] + 10)] = secondary;
-    leds[(cluster[1] + 12)] = secondary;
-  }
-}
-
-/*
-void fadeCluster(int cluster[], CRGB primary, CRGB secondary)
-{
-  if (cluster[0] == 1) {
-    fadeUsingColor(cluster[1], NUM_LEDS, primary);
-    fadeUsingColor((cluster[1] + 1), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 2), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 3), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 4), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 5), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 6), NUM_LEDS, secondary);
-  } else {
-    fadeUsingColor(cluster[1], NUM_LEDS, primary);
-    fadeUsingColor((cluster[1] + 2), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 4), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 6), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 8), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 10), NUM_LEDS, secondary);
-    fadeUsingColor((cluster[1] + 12), NUM_LEDS, secondary);
-  }
-}
-*/
-
-/*
- * Mapping
- */
-void test()
-{
-  FastLED.clear();
-  for( int i = 0; i < NUM_CLUSTERS; i++) {
-    setCluster(barnacles[i], CHSV(0, 0, 255), CHSV(165, 255, 255));
-  }
-}
-
-void background()
+void ambient()
 {
   fadeToBlackBy(leds, NUM_LEDS, 6);
-  int pos = random8(NUM_CLUSTERS);
-  setCluster(barnacles[pos], CHSV(64, 255, 255), CHSV(random8(120, 180), 255, 255));
+  int pos = random8(NUM_BARNACLES);
+  barnacles[pos]->setCluster(CHSV(0, 255, 255), CHSV(random8(130, 165), 255, 255));
 }
 
-void illuminate(int section)
+void illuminate(int section[])
 {
   fadeToBlackBy(leds, NUM_LEDS, 3);
-  int pos = random8(sections[section][0], sections[section][1]);
-  setCluster(barnacles[pos], CHSV(0, 255, 255), CHSV(45, 255, 255));
+  int pos = random8(section[0], section[1]);
+  barnacles[pos]->setCluster(CHSV(0, 255, 255), CHSV(random8(36, 64), 255, 255));
 }
 
-/*
- * Efect #1
- */
-void rainbowWithGlitter() 
+void lightning(int start) 
 {
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
-}
-
-void rainbow() 
-{
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-}
-
-void addGlitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  int up = start;
+  int down = start;
+  
+  for(int i = 0; i < NUM_BARNACLES; i++) {
+    up = up + 1;
+    down = down - 1;
+    barnacles[up]->setCluster(CHSV(0, 255, 255), CHSV(45, 255, 255));
+    barnacles[down]->setCluster(CHSV(0, 255, 255), CHSV(45, 255, 255));
+  
   }
 }
-
-
-/*
- * Efect #2
- */
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = FRAMES_PER_SECOND;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_CLUSTERS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-
-/*
- * Efect #3
- */
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-}
-
